38b086359ffa4e08ea9811abe7bf8d87fcd40419
Ksieon <keumsiun0503@gmail.com>
2025-08-30 19:24:23 +0900

    :tada: Start: init repository setting

---
+++ src/test/java/com/skthon/nayngpunch/NayngpunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skthon.nayngpunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NayngpunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
